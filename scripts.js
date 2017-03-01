e50c12a0268c1f6550cffb8d7208027bc8ee6650
Lane Harvey <mrlaneharvey@gmail.com>
2017-02-28 18:41:02 -0700

    displayTimeLeft function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer (seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -14,4 +16,12 @@ function timer (seconds) {
     // display it
     console.log(secondsLeft);
   }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
 }
